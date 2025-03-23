e499f6a3af9364a567287a6470386c691734da4d
Nikolay Vyahhi <nikolay.vyahhi@hyperskill.org>
2025-03-23 13:41:23 -0400

    maestro demo

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# big-red-button
+# Big Red Button
+
+We test and break everything, agentically.
+
+## Installation
 
 ```
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
+```
+
+## Usage
+
+```python
+python main.py
 ```
--- main.py
+++ main.py
@@ -9,15 +9,18 @@ client = AI21Client(
     api_key=os.environ.get('AI21_API_KEY'),
 )
 
-system = "You're a support engineer in a SaaS company"
-messages = [
-    ChatMessage(content=system, role="system"),
-    ChatMessage(content="Hello, I need help with a signup process.", role="user"),
-]
-
-chat_completions = client.chat.completions.create(
-    messages=messages,
-    model="jamba-mini-1.6-2025-03",
+run_result = client.beta.maestro.runs.create_and_poll(
+    input="Write a poem about the ocean",
+    requirements=[
+        {
+            "name": "length requirement",
+            "description": "The length of the poem should be less than 1000 characters",
+        },
+        {
+            "name": "rhyme requirement",
+            "description": "The poem should rhyme",
+        },
+    ],
 )
 
-print(chat_completions)
+print(run_result.result)
